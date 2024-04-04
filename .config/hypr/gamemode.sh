3341f8802fc6940b359c0863ad558c1a9713f74a
Lluís <lluisalillu@icloud.com>
2024-04-05 01:09:22 +0200

    Hyprland config better powersaving and better animations

--- .config/hypr/gamemode.sh
+++ .config/hypr/gamemode.sh
@@ -9,6 +9,6 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
-    exit
+   exit
 fi
 hyprctl reload
