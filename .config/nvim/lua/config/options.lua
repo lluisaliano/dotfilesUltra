755e3420c558026b4bc2bcb8999743d15cc7771b
Lluís Aliaño Llufriu <lluisalillu@icloud.com>
2026-02-12 11:39:27 +0100

    Changed vicinae overlay, and adde clangd and new theme to nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Keep LazyVim format-on-save enabled globally.
+vim.g.autoformat = true
